714616776b0ad622ed9f3cbb708b443f10c90453
exophrenik <exophrenik@gmail.com>
2021-04-16 11:03:56 +0300

    Implemented User permissions.

--- src/main/java/com/apboutos/moneytrackapi/model/User.java
+++ src/main/java/com/apboutos/moneytrackapi/model/User.java
@@ -2,7 +2,9 @@ package com.apboutos.moneytrackapi.model;
 
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,8 +13,9 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
+
+import static com.apboutos.moneytrackapi.model.User.UserRolePermission.*;
 
 @Entity
 @NoArgsConstructor
@@ -85,11 +88,6 @@ public class User implements UserDetails {
         return enabled;
     }
 
-    public enum UserRole{
-        USER,
-        ADMIN
-    }
-
     public Boolean confirmProperDatabaseSave(User databaseSavedUser){
 
         return this.username.equals(databaseSavedUser.getUsername()) &&
@@ -100,4 +98,24 @@ public class User implements UserDetails {
                 this.locked.equals(databaseSavedUser.locked) &&
                 this.enabled.equals(databaseSavedUser.enabled);
     }
+
+    @RequiredArgsConstructor
+    public enum UserRole{
+        USER(new HashSet<>(Arrays.asList(USER_READ,ENTRY_READ,ENTRY_WRITE,CATEGORY_READ,CATEGORY_WRITE))),
+        ADMIN(new HashSet<>(Arrays.asList(USER_READ,USER_WRITE,ENTRY_READ,ENTRY_WRITE,CATEGORY_READ,CATEGORY_WRITE)));
+
+        @Getter
+        private final Set<UserRolePermission> permissions;
+
+    }
+
+    public enum UserRolePermission{
+        USER_WRITE,
+        USER_READ,
+        ENTRY_WRITE,
+        ENTRY_READ,
+        CATEGORY_WRITE,
+        CATEGORY_READ
+    }
+
 }
