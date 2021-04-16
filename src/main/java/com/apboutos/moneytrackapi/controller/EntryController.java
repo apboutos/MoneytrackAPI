e95c359922125cf0f156406db1ff9a7c87cd75b7
exophrenik <exophrenik@gmail.com>
2021-04-16 12:19:56 +0300

    EntryController.java implementation.

---
+++ src/main/java/com/apboutos/moneytrackapi/controller/EntryController.java
@@ -0,0 +1,27 @@
+package com.apboutos.moneytrackapi.controller;
+
+import com.apboutos.moneytrackapi.controller.Response.EntryPostResponse;
+import com.apboutos.moneytrackapi.model.Entry;
+import com.apboutos.moneytrackapi.service.EntryService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/api/v1/entries", produces = "application/json")
+@AllArgsConstructor
+public class EntryController {
+
+    private final EntryService entryService;
+
+    @PutMapping
+    EntryPostResponse saveEntries(@RequestBody List<Entry> entries){
+
+        List<List<Entry>> updateResults = entryService.saveEntries(entries);
+        return new EntryPostResponse("201","Success","Entries updated." , updateResults.get(0),updateResults.get(1),updateResults.get(2));
+    }
+}
