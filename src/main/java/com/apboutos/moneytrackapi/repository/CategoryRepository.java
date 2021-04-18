627b4e53fe427e982d3909085a68f8f0d233180a
exophrenik <exophrenik@gmail.com>
2021-04-18 11:46:13 +0300

    findCategoriesByUser implementation.

--- src/main/java/com/apboutos/moneytrackapi/repository/CategoryRepository.java
+++ src/main/java/com/apboutos/moneytrackapi/repository/CategoryRepository.java
@@ -2,18 +2,20 @@ package com.apboutos.moneytrackapi.repository;
 
 import com.apboutos.moneytrackapi.model.Category;
 import com.apboutos.moneytrackapi.model.CategoryId;
-import com.apboutos.moneytrackapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@SuppressWarnings("unused")
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, CategoryId> {
 
     Optional<Category> findCategoryById(CategoryId id);
 
-    List<Category> findCategoriesByUsers(List<User> users);
+    @Query("select category from Category category join category.users users where users.username =:username")
+    List<Category> findCategoriesByUser(String username);
 
 }
