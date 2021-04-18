f796a920d2eec9f81fb425eca30efef3ef4a4b34
exophrenik <exophrenik@gmail.com>
2021-04-18 11:44:55 +0300

    CategoryDTO.java implementation.

---
+++ src/main/java/com/apboutos/moneytrackapi/model/CategoryDTO.java
@@ -0,0 +1,14 @@
+package com.apboutos.moneytrackapi.model;
+
+import lombok.Data;
+
+import static com.apboutos.moneytrackapi.model.Entry.*;
+
+@Data
+public class CategoryDTO {
+
+    private final String name;
+    private final Type type;
+    private final String username;
+
+}
