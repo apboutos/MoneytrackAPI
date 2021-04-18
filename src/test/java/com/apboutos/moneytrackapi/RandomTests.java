23c3bc110ec3c182c08e2f8f9b5a41e0899680b9
exophrenik <exophrenik@gmail.com>
2021-04-18 12:45:49 +0300

    BcryptTest

--- src/test/java/com/apboutos/moneytrackapi/RandomTests.java
+++ src/test/java/com/apboutos/moneytrackapi/RandomTests.java
@@ -4,6 +4,7 @@ import com.apboutos.moneytrackapi.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.sql.Time;
 import java.sql.Timestamp;
@@ -19,6 +20,13 @@ public class RandomTests {
         User user = new User();
         System.out.println(user.getUsername());
         System.out.println(Timestamp.from(Instant.now()));
+    }
+
+    @Test
+    public void BCryptTest(){
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("ma582468"));
     }
 
 
