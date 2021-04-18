d67aaa19cf116d1ce5e8c5bad94ba76b42a73da6
exophrenik <exophrenik@gmail.com>
2021-04-18 08:41:09 +0300

    Implemented ManyToMany relationship and added a type field.

--- src/main/java/com/apboutos/moneytrackapi/model/Category.java
+++ src/main/java/com/apboutos/moneytrackapi/model/Category.java
@@ -1,18 +1,22 @@
 package com.apboutos.moneytrackapi.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Set;
+
+import static com.apboutos.moneytrackapi.model.Entry.*;
 
 @Data
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
 public class Category {
     @Id
     @Size(max = 45)
@@ -20,7 +24,15 @@ public class Category {
     private String name;
     private final Timestamp createdAt = Timestamp.from(Instant.now());
 
-    public Category(String name){
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Type type;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<User> users;
+
+    public Category(String name, Type type){
         this.name = name;
+        this.type = type;
     }
 }
