90804d637796bdce20cc5dda79ad5008e6437a8d
exophrenik <exophrenik@gmail.com>
2021-05-22 11:53:19 +0300

    Code refactoring. -Deleted commented code.

--- src/main/java/com/apboutos/moneytrackapi/model/Category.java
+++ src/main/java/com/apboutos/moneytrackapi/model/Category.java
@@ -28,27 +28,4 @@ public class Category {
         this.id = id;
     }
 
-     /*
-        We cannot use lombok default implementation of equals and hashcode because lombok uses the list of users
-        inside the hashcode implementation, and this creates a stackoverflow error due to recursive call of hashcode
-        between User and Category.
-     */
-    /*
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Category category = (Category) o;
-        return id.equals(category.id) && createdAt.equals(category.createdAt);
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-
-        hash = 31 * hash + (id == null ? 0 : id.hashCode());
-        hash = 31 * hash + (createdAt == null ? 0 : createdAt.hashCode());
-
-        return hash;
-    }*/
 }
