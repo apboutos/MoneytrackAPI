eb0fb9f9ddf38b99cf718347db1ece0920b97ee3
exophrenik <exophrenik@gmail.com>
2021-05-23 11:42:06 +0300

    Implementation. -GetEntriesResponse implemented.

---
+++ src/main/java/com/apboutos/moneytrackapi/controller/Response/GetEntriesResponse.java
@@ -0,0 +1,21 @@
+package com.apboutos.moneytrackapi.controller.Response;
+
+import com.apboutos.moneytrackapi.controller.dto.EntryDTO;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@ResponseStatus
+public class GetEntriesResponse {
+
+    private final HttpStatus status;
+    private final String result;
+    private final String message;
+    private final Timestamp timestamp;
+    private final List<EntryDTO> savedEntries;
+
+}
