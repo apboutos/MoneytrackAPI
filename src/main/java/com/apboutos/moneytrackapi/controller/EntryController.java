da0c3479c135ff16331e2da7f711e0860e448284
exophrenik <exophrenik@gmail.com>
2021-05-23 11:41:49 +0300

    Implementation. -getEntries implemented.

--- src/main/java/com/apboutos/moneytrackapi/controller/EntryController.java
+++ src/main/java/com/apboutos/moneytrackapi/controller/EntryController.java
@@ -1,17 +1,16 @@
 package com.apboutos.moneytrackapi.controller;
 
 import com.apboutos.moneytrackapi.controller.Response.CreateEntriesResponse;
+import com.apboutos.moneytrackapi.controller.Response.GetEntriesResponse;
 import com.apboutos.moneytrackapi.controller.dto.EntryDTO;
 import com.apboutos.moneytrackapi.service.EntryService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
@@ -25,6 +24,13 @@ public class EntryController {
     ResponseEntity<CreateEntriesResponse> createEntries(@Valid @RequestBody List<EntryDTO> entries, Authentication authentication) {
 
         CreateEntriesResponse response = entryService.saveEntries(entries,authentication.getName());
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+
+    @GetMapping
+    ResponseEntity<GetEntriesResponse> getEntries(@RequestParam("lastPullRequestTimestamp") @NotBlank String lastPullRequestTimestamp, Authentication authentication){
+
+        GetEntriesResponse response = entryService.getEntries(lastPullRequestTimestamp,authentication.getName());
         return new ResponseEntity<>(response,response.getStatus());
     }
 
