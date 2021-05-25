55ca766a0777183ec3e9df9fc5cae5602fbba5c7
exophrenik <exophrenik@gmail.com>
2021-05-25 19:35:07 +0300

    Code refactoring. -Deleted update Method test.

--- src/test/java/com/apboutos/moneytrackapi/repository/EntryRepositoryTest.java
+++ src/test/java/com/apboutos/moneytrackapi/repository/EntryRepositoryTest.java
@@ -3,7 +3,6 @@ package com.apboutos.moneytrackapi.repository;
 import com.apboutos.moneytrackapi.model.Category;
 import com.apboutos.moneytrackapi.model.Entry;
 import com.apboutos.moneytrackapi.model.User;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,13 +12,12 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
-import static com.apboutos.moneytrackapi.model.Entry.Type.Expense;
 import static java.sql.Timestamp.from;
 import static java.time.Instant.now;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@SuppressWarnings("OptionalGetWithoutIsPresent")
+
 @DataJpaTest
 class EntryRepositoryTest {
 
@@ -91,29 +89,4 @@ class EntryRepositoryTest {
         assertThat(actualEntriesAfterFirst.get(1)).isEqualTo(entry3);
     }
 
-    @Test
-    @Disabled
-    void update() {
-    //TODO this test fails and needs to be worked.
-        //given
-        User testUser = userRepository.save(new User("testUser@test.com","000000000000000000000000000000000000000000000000000000000000"));
-        Category testCategory = categoryRepository.save(new Category("Test Category", Entry.Type.Income,testUser));
-        Entry testEntry = new Entry("testEntry",testUser, Entry.Type.Income,
-                testCategory, "test",
-                0.0, new Date(343), from(now()), false);
-        System.out.println(testCategory);
-        entryRepositoryUnderTest.save(testEntry);
-
-        //when
-        entryRepositoryUnderTest.update("testEntry",testUser,Expense,testCategory,"testUpdated",1.0,new Date(346), from(now()),true);
-        Optional<Entry> actual = entryRepositoryUnderTest.findEntryById("testEntry");
-        System.out.println(actual.get().getCategory());
-        //then
-        assertThat(actual.get().getUsername()).isEqualTo(testUser);
-        assertThat(actual.get().getType()).isEqualTo(Expense);
-        assertThat(actual.get().getCategory()).isEqualTo(testCategory);
-        assertThat(actual.get().getDescription()).isEqualTo("testUpdated");
-        assertThat(actual.get().getAmount()).isEqualTo(1.0);
-        assertThat(actual.get().getIsDeleted()).isEqualTo(true);
-    }
 }
