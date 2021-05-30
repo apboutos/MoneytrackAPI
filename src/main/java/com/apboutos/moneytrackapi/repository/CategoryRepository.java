f44ba173df623239244ee373ff24c94989cab0b0
exophrenik <exophrenik@gmail.com>
2021-05-30 12:12:06 +0300

    Implementation. -updateCategory implementation.

--- src/main/java/com/apboutos/moneytrackapi/repository/CategoryRepository.java
+++ src/main/java/com/apboutos/moneytrackapi/repository/CategoryRepository.java
@@ -4,6 +4,9 @@ import com.apboutos.moneytrackapi.model.Category;
 import com.apboutos.moneytrackapi.model.CategoryId;
 import com.apboutos.moneytrackapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +19,9 @@ public interface CategoryRepository extends JpaRepository<Category, CategoryId> 
     Optional<Category> findCategoryById(CategoryId id);
 
     List<Category> findCategoriesByUser(User user);
+
+    @Modifying(clearAutomatically = true,flushAutomatically = true)
+    @Query("UPDATE Category c SET c = :category WHERE c.id = :id")
+    void updateCategory(@Param(value = "category") Category category, @Param(value = "id") CategoryId id);
 
 }
