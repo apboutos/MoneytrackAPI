67e76143801c1985a184aec87f41ca3a3f948c25
exophrenik <exophrenik@gmail.com>
2021-05-30 12:12:20 +0300

    Implementation. -updateCategory implementation.

--- src/test/java/com/apboutos/moneytrackapi/repository/CategoryRepositoryTest.java
+++ src/test/java/com/apboutos/moneytrackapi/repository/CategoryRepositoryTest.java
@@ -4,6 +4,7 @@ import com.apboutos.moneytrackapi.model.Category;
 import com.apboutos.moneytrackapi.model.CategoryId;
 import com.apboutos.moneytrackapi.model.Entry;
 import com.apboutos.moneytrackapi.model.User;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -65,4 +66,38 @@ class CategoryRepositoryTest {
         //then
         assertThat(new HashSet<>(actualCategories)).isEqualTo(testCategoriesSet);
     }
+
+    @Disabled
+    @Test
+    void updateCategory(){
+
+        //given
+        User testUser = new User("username@test.com","123456789012345678901234567890123456789012345678901234567890");
+        userRepository.save(testUser);
+        User testUser2 = new User("username2@test.com","123456789012345678901234567890123456789012345678901234567890");
+        userRepository.save(testUser2);
+        Category category1 = new Category("TestCategory1", Entry.Type.Income,testUser);
+        Category category2 = new Category("TestCategory2", Entry.Type.Income,testUser);
+        Category category3 = new Category("TestCategory3", Entry.Type.Income,testUser);
+        Category category4 = new Category("TestCategory1", Entry.Type.Expense,testUser);
+        Category category5 = new Category("TestCategory5", Entry.Type.Expense,testUser);
+        Category category6 = new Category("TestCategory6", Entry.Type.Expense,testUser2);
+
+        categoryRepositoryUnderTest.save(category1);
+        categoryRepositoryUnderTest.save(category2);
+        categoryRepositoryUnderTest.save(category3);
+
+        //when
+        categoryRepositoryUnderTest.updateCategory(category4,category1.getId());
+        categoryRepositoryUnderTest.updateCategory(category5,category2.getId());
+        categoryRepositoryUnderTest.updateCategory(category6,category3.getId());
+
+        //then
+        assertThat(categoryRepositoryUnderTest.findCategoryById(category1.getId()).get().getId().getType()).isEqualTo(Entry.Type.Expense);
+        assertThat(categoryRepositoryUnderTest.findCategoryById(category2.getId())).isEmpty();
+        assertThat(categoryRepositoryUnderTest.findCategoryById(category5.getId())).isNotEmpty();
+        assertThat(categoryRepositoryUnderTest.findCategoryById(category6.getId()).get().getUser()).isEqualTo(testUser2);
+
+    }
+
 }
